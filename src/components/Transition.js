2c351270325a04738accd72595b37788376163a7
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-11 08:32:43 +0100

    using react-transition-group module

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
 import '../css/App.css';
 
 class TransitionComp extends Component{
@@ -21,11 +22,24 @@ class TransitionComp extends Component{
 
         return(
             <div>
-                    <div style = {{
-                        background:'red',
-                        height: '80px', 
-                        textAlign: 'center'
-                    }}> Transition, <br /> state = {`${this.state.show}`} </div>
+                {/* <Transition/> wrapps around the element to display the effect and returns them via a func */}
+                {/* takes in props of in and timeout */}
+                <Transition
+                    in={this.state.show}
+                    timeout={2000}
+                >
+
+                    { (transitionState) => 
+                         <div style = {{
+                            background:'red',
+                            height: '80px', 
+                            textAlign: 'center',
+                            color: 'cyan'
+                        }}> Transition, <br /> state = {`${this.state.show}`} <br /> trans-state = {transitionState} </div>
+                    }
+
+                </Transition>
+                    
                 <button onClick = {this.toggleDiv}> Toggle show / hide </button>
             </div>
 
@@ -36,3 +50,9 @@ class TransitionComp extends Component{
 
 
 export default TransitionComp;
+
+// NB: Transition do not do the animations the return a function that help you with monitoring
+// the state of the transition as it occurs, ENTERING, ENTERED, EXITING, EXITED
+
+// it also does not return a div / Element, it returns a function that return the jsx (like noticed in hoc)..
+// this returned function can take in an arg used to monitor the state of the transition at a given moment
