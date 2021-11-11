02db1359406549d969e9024017db5b1d1487eea5
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-11 17:35:31 +0100

    replicated transition with CSSTransition import

--- src/components/CSStransition.js
+++ src/components/CSStransition.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-
+import { CSSTransition } from 'react-transition-group';
 import '../css/App.css';
 
 class Fade extends Component{
+
+    state = {
+        show: true
+    }
+
+    toggleDiv = () => {
+        this.setState({
+            show: this.state.show ? false : true
+        })
+    }
+
+
     render(){
         return(
             <div>
-                CSS TRANSITIONS
+
+                <CSSTransition
+                    in={this.state.show}
+                    timeout={2000}
+                    classNames={'boxed'}
+                >
+
+                    <div className='boxed'>
+                        CSS TRANSITIONS
+                    </div>
+
+                </CSSTransition>
+
+                <button onClick = {this.toggleDiv} className = 'showDiv'> Toggle show / hide </button>
+                
             </div>
         )
     }
