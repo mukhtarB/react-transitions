eb0d7f75acfa9021030bbee9d02fc8f0740b1a5b
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-11 08:02:11 +0100

    Set up component state to track transition-state and method for toggling the component state

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -3,15 +3,34 @@ import '../css/App.css';
 
 class TransitionComp extends Component{
 
+    // defining default state
+    state = {
+        show: true
+    }
+
+    // func to toggle state of div -> on click of toggle button
+    // not using --(a), since direct manipulation of the original content of the state is considered bad practice
+    toggleDiv = () => {
+        this.setState({
+            // show: !this.state.show ------ (a)
+            show: this.state.show ? false : true
+        })
+    }
+
     render(){
 
         return(
             <div>
-                <div> Transition </div>
-                <button> Toggle show / hide </button>
+                    <div style = {{
+                        background:'red',
+                        height: '80px', 
+                        textAlign: 'center'
+                    }}> Transition, <br /> state = {`${this.state.show}`} </div>
+                <button onClick = {this.toggleDiv}> Toggle show / hide </button>
             </div>
 
         )
+
     }
 }
 
