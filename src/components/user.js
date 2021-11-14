9cd92d7ad460cb703f00fc4ae493b088ff845065
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-14 08:39:52 +0100

    Created User component

---
+++ src/components/user.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class User extends Components {
+    render () {
+        return (
+            <div>
+                User Class Component
+            </div>
+        )
+    }
+}
+
+export default User;
